2f5b064eb450a5e474e3b74a0e96a2323db2cfec
XiaojunLiang1 <nickl0802@outlook.com>
2024-08-15 23:39:28 +1000

    week3 and 4 efoilo

--- week2/xliang-library/src/App.vue
+++ week2/xliang-library/src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import JSON from './components/JSON.vue';
+import Form from './components/Form.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <JSON />
-  </main>
+   <!-- <JSON /> --> 
+   <Form />
 </template>
 
 <style scoped>
